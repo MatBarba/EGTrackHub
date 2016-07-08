e31d77fe3f1dfce1287981f6b676737f1085b67e
Matthieu Barba <mbarba@ebi.ac.uk>
2016-07-08 16:12:49 +0100

    Convert AEStudy to Moose and use the new ArrayExpress

--- modules/EGTH/AEStudy.pm
+++ modules/EGTH/AEStudy.pm
@@ -2,6 +2,7 @@ package EGTH::AEStudy;
 
 use strict;
 use warnings;
+use Moose;
 
 use Date::Manip;
 
@@ -12,94 +13,76 @@ use EGTH::ENA;
 ## this is a class of an AE study. It considers only PLANT species.
 # AE REST call: http://www.ebi.ac.uk/fg/rnaseq/api/json/70/getRunsByStudy/SRP068911
 
-sub new {
+has study_id => (
+  is       => 'ro',
+  isa      => 'Str',
+  required => 1,
+);
 
-  my $class = shift;
+has plant_names => (
+  is       => 'ro',
+  isa      => 'ArrayRef[Str]',
+  required => 1,
+);
 
-  my $study_id = shift;
-  my $plant_names_response_href = shift;
+has runs => (
+  is       => 'ro',
+  isa      => 'HashRef[HashRef]',
+  lazy    => 1,
+  builder => 'make_runs',
+);
 
-  defined $study_id and $plant_names_response_href
-    or die "Some required parameters are missing in the constructor of the AEStudy\n";
-
-  my $self = {
-    study_id => $study_id,
-    plant_names => $plant_names_response_href
-  };
-
-  my $run_tuple_of_study_ref =  make_runs_tuple_plants_of_study($self);
-  $self->{run_tuple} = $run_tuple_of_study_ref;
-
-  return bless $self, $class; #this is what makes a reference into an object
-}
-
-sub make_runs_tuple_plants_of_study {  
-
+sub make_runs {
   my $self = shift;
 
-  my $study_id = $self->{study_id};
-  my $plant_names_response_href = $self->{plant_names}; # EGTH::ArrayExpress::get_plant_names_AE_API();
+  my %plant_names = map { $_ => 1 } @{ $self->plant_names };
 
   my %run_tuple; # to be returned
-
-  my %plant_names_AE = %{$plant_names_response_href};  # gives all distinct plant names with processed runs by AE
   
-  my $runs_response = EGTH::ArrayExpress::get_runs_json_for_study($study_id);
-  my @runs_json; # returns list of hash references
-
-  if ($runs_response ==0){
-
-    die "Could not get runs for study $study_id using AE REST call /getRunsByStudy/$study_id in AEStudy module\n";
-
-  }else{
-
-    @runs_json = @{$runs_response};
-  }
-
-# a response stanza (the response is usually more than 1 stanza, 1 study has many bioreps, each stanza is a biorep) of this call:  http://www.ebi.ac.uk/fg/rnaseq/api/json/70/getRunsByStudy/SRP033494
-#[{"STUDY_ID":"SRP033494","SAMPLE_IDS":"SAMN02434874","BIOREP_ID":"SRR1042754","RUN_IDS":"SRR1042754","ORGANISM":"arabidopsis_thaliana","REFERENCE_ORGANISM":"arabidopsis_thaliana","STATUS":"Complete",
-#"ASSEMBLY_USED":"TAIR10","ENA_LAST_UPDATED":"Fri Jun 19 2015 18:11:03","LAST_PROCESSED_DATE":"Sun Nov 15 2015 00:31:20",
-#"CRAM_LOCATION":"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/SRR104/004/SRR1042754/SRR1042754.cram"},
-
-# or with merges of CRAMs
-
-#[{"STUDY_ID":"SRP021098","SAMPLE_IDS":"SAMN02799120","BIOREP_ID":"E-MTAB-4045.biorep54","RUN_IDS":"SRR1298603,SRR1298604","ORGANISM":"glycine_max","REFERENCE_ORGANISM":"glycine_max",
-#"STATUS":"Complete","ASSEMBLY_USED":"V1.0","ENA_LAST_UPDATED":"Fri Jun 19 2015 18:53:48","LAST_PROCESSED_DATE":"Mon Jan 25 2016 16:46:04",
-#"CRAM_LOCATION":"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/aggregated_techreps/E-MTAB-4045/E-MTAB-4045.biorep54.cram","MAPPING_QUALITY":77},
-
-# there are cases of null sample ids: (I want to skip those track hubs)
-#[{"STUDY_ID":"DRP002805","SAMPLE_IDS":null,"BIOREP_ID":"DRR048597","RUN_IDS":"DRR048597","ORGANISM":"brachypodium_distachyon","REFERENCE_ORGANISM":"brachypodium_distachyon","STATUS":"Complete",
-#"ASSEMBLY_USED":"v1.0","ENA_LAST_UPDATED":"Fri Mar 11 2016 01:37:14","LAST_PROCESSED_DATE":"Sat Mar 12 2016 22:48:54","CRAM_LOCATION":"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/DRR048/DRR048597/DRR048597.cram","MAPPING_QUALITY":88},
-
-  foreach my $run_stanza (@runs_json){
-    
-    if($run_stanza->{"STATUS"} eq "Complete" and $plant_names_AE{$run_stanza->{"REFERENCE_ORGANISM"}}){
-
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"sample_ids"}=$run_stanza->{"SAMPLE_IDS"}; # ie $run{"SRR1042754"}{"sample_ids"}="SAMN02434874,SAMN02434875"
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"organism"}=$run_stanza->{"REFERENCE_ORGANISM"};
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"assembly_name"}=$run_stanza->{"ASSEMBLY_USED"};  #ie "TAIR10"
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"big_data_file_server_location"}=$run_stanza->{"CRAM_LOCATION"};
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"AE_processed_date"}=$run_stanza->{"LAST_PROCESSED_DATE"};
-      $run_tuple{$run_stanza->{"BIOREP_ID"}}{"run_ids"}=$run_stanza->{"RUN_IDS"};
- 
+  # Get the runs
+  my $runs_response = EGTH::ArrayExpress::get_runs_json_for_study( $self->study_id );
+  my @runs_json = @{$runs_response};
+  
+  # Get the fields
+  my %fmap = (
+      sample_ids                    => 'SAMPLE_IDS', # ie $run{ SRR1042754 }{ sample_ids }="SAMN02434874,SAMN02434875"
+      organism                      => 'REFERENCE_ORGANISM',
+      assembly_name                 => 'ASSEMBLY_USED',  #ie "TAIR10"
+      big_data_file_server_location => 'CRAM_LOCATION',
+      AE_processed_date             => 'LAST_PROCESSED_DATE',
+      run_ids                       => 'RUN_IDS',
+  );
+  
+  my %runs;
+  foreach my $stanza (@runs_json) {
+    my $plant_ref = $stanza->{"REFERENCE_ORGANISM"};
+    if(
+      $stanza->{"STATUS"} eq "Complete"
+        and $plant_names{ $plant_ref }
+    ){
+      my %run;
+      for my $run_field (keys %fmap) {
+        my $stanza_field = $fmap{ $run_field };
+        $run{ $run_field } = $stanza->{ $stanza_field };
+      }
+      my $biorep_id = $stanza->{"BIOREP_ID"};
+      $runs{ $biorep_id } = \%run;
     }
   }
-  return \%run_tuple;
-  
+  return \%runs;
 }
 
-sub id{
-
+sub id {
   my $self = shift;
-  return $self->{study_id};
+  return $self->study_id;
 }
 
-sub get_biorep_ids_by_organism{ # this method is used when there is a study with many assemblies (or organisms); I can get the biorep ids of a specific organism of the study
-
+# this method is used when there is a study with many assemblies (or organisms); I can get the biorep ids of a specific organism of the study
+sub get_biorep_ids_by_organism{
   my $self = shift;
-  my $organism_name = shift;
+  my ($organism_name) = @_;
 
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
   my %biorep_ids;
 
   foreach my $biorep_id (keys %{$run_tuple}){
@@ -116,7 +99,7 @@ sub get_organism_names_assembly_names{
 
   my $self = shift;
 
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
   my %organism_names;
   my $organism_name;
 
@@ -135,7 +118,7 @@ sub get_sample_ids{
 
   my $self = shift;
 
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
   my %sample_ids;
 
   my %biorep_ids = %{$self->get_biorep_ids};
@@ -160,7 +143,7 @@ sub get_assembly_name_from_biorep_id{
 
   my $self=shift;
   my $biorep_id = shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
     
   my $assembly_name =$run_tuple->{$biorep_id}{"assembly_name"};
   return $assembly_name;
@@ -170,7 +153,7 @@ sub get_sample_ids_from_biorep_id{
 
   my $self = shift;
   my $biorep_id = shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
    
   my @sample_ids= split (/,/,$run_tuple->{$biorep_id}{"sample_ids"});
   
@@ -180,7 +163,7 @@ sub get_sample_ids_from_biorep_id{
 sub get_biorep_ids{
 
   my $self=shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   my %biorep_ids;
 
@@ -196,7 +179,7 @@ sub get_biorep_ids_from_sample_id{
   my $self=shift;
   my $sample_id = shift;
 
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   my %biorep_ids;
 
@@ -219,7 +202,7 @@ sub get_biorep_ids_from_sample_id{
 sub get_assembly_names{  # of study
 
   my $self=shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   my %assembly_names;
 
@@ -238,7 +221,7 @@ sub get_big_data_file_location_from_biorep_id {
   my $self=shift;
   my $biorep_id = shift;
 
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
   
   if(!$run_tuple->{$biorep_id}){
     die "There is not such biorep id $biorep_id in study ".$self->{study_id}."\n";
@@ -252,7 +235,7 @@ sub get_AE_last_processed_date_from_biorep_id {
 
   my $self=shift;
   my $biorep_id = shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   if(!$run_tuple->{$biorep_id}){
     die "There is not such biorep id $biorep_id in study ".$self->{study_id}."\n";
@@ -266,7 +249,7 @@ sub get_run_ids_of_biorep_id{  # could be more than 1 run id : "RUN_IDS":"DRR001
 
   my $self=shift;
   my $biorep_id = shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   if(!$run_tuple->{$biorep_id}){
     die "There is not such biorep id $biorep_id in study ".$self->{study_id}."\n";
@@ -283,7 +266,7 @@ sub give_big_data_file_type_of_biorep_id{
 
   my $self=shift;
   my $biorep_id = shift;
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   if(!$run_tuple->{$biorep_id}){
     die "There is not such biorep id $biorep_id in study ".$self->{study_id}."\n";
@@ -297,12 +280,13 @@ sub give_big_data_file_type_of_biorep_id{
 
 }
 
-sub get_AE_last_processed_unix_date{  # of the study : i get all its bioreps and then find the max date of all bioreps # tried with this study: http://www.ebi.ac.uk/fg/rnaseq/api/json/70/getRunsByStudy/SRP067728
+# of the study : i get all its bioreps and then find the max date of all bioreps # tried with this study: http://www.ebi.ac.uk/fg/rnaseq/api/json/70/getRunsByStudy/SRP067728
+sub get_AE_last_processed_unix_date{
 
   my $self= shift;
 
   my %biorep_ids = %{$self->get_biorep_ids};
-  my $run_tuple = $self->{run_tuple};
+  my $run_tuple = $self->runs;
 
   my $max_date=0;
 
@@ -321,3 +305,60 @@ sub get_AE_last_processed_unix_date{  # of the study : i get all its bioreps and
 }
 
 1;
+
+__END__
+
+# a response stanza (the response is usually
+#  more than 1 stanza,
+# 1 study has many bioreps,
+#  each stanza is a biorep) of this call:
+#  http://www.ebi.ac.uk/fg/rnaseq/api/json/70/getRunsByStudy/SRP033494
+# [{
+# "STUDY_ID":"SRP033494",
+# "SAMPLE_IDS":"SAMN02434874",
+# "BIOREP_ID":"SRR1042754",
+# "RUN_IDS":"SRR1042754",
+# "ORGANISM":"arabidopsis_thaliana",
+# "REFERENCE_ORGANISM":"arabidopsis_thaliana",
+# "STATUS":"Complete",
+#"ASSEMBLY_USED":"TAIR10",
+#"ENA_LAST_UPDATED":"Fri Jun 19 2015 18:11:03",
+#"LAST_PROCESSED_DATE":"Sun Nov 15 2015 00:31:20",
+#"CRAM_LOCATION":
+#"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/SRR104/004/SRR1042754/SRR1042754.cram"
+#},
+
+# or with merges of CRAMs
+
+#[{
+#"STUDY_ID":"SRP021098",
+#"SAMPLE_IDS":"SAMN02799120",
+#"BIOREP_ID":"E-MTAB-4045.biorep54",
+#"RUN_IDS":"SRR1298603,SRR1298604",
+#"ORGANISM":"glycine_max",
+#"REFERENCE_ORGANISM":"glycine_max",
+#"STATUS":"Complete",
+#"ASSEMBLY_USED":"V1.0",
+#"ENA_LAST_UPDATED":"Fri Jun 19 2015 18:53:48",
+#"LAST_PROCESSED_DATE":"Mon Jan 25 2016 16:46:04",
+#"CRAM_LOCATION":
+#"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/aggregated_techreps/E-MTAB-4045/E-MTAB-4045.biorep54.cram",
+#"MAPPING_QUALITY":77
+#},
+
+# there are cases of null sample ids: (I want to skip those track hubs)
+#[{
+#"STUDY_ID":"DRP002805",
+#"SAMPLE_IDS":null,
+#"BIOREP_ID":"DRR048597",
+#"RUN_IDS":"DRR048597",
+#"ORGANISM":"brachypodium_distachyon",
+#"REFERENCE_ORGANISM":"brachypodium_distachyon",
+#"STATUS":"Complete",
+#"ASSEMBLY_USED":"v1.0",
+#"ENA_LAST_UPDATED":"Fri Mar 11 2016 01:37:14",
+#"LAST_PROCESSED_DATE":"Sat Mar 12 2016 22:48:54",
+#"CRAM_LOCATION":
+#"ftp://ftp.ebi.ac.uk/pub/databases/arrayexpress/data/atlas/rnaseq/DRR048/DRR048597/DRR048597.cram",
+#"MAPPING_QUALITY":88
+#},
