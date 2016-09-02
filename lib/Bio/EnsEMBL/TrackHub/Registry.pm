eea6072f817b545b764cc2b32e75b5efabc00d19
Matthieu Barba <mbarba@ebi.ac.uk>
2016-09-02 09:31:18 +0100

    Review Registry module and subroutines: doc and POD
    * Added dev comment for each subroutine
    * Added POD
    * Changed some subroutines, e.g.
    - register_track_hub -> register_track_hub_data
    - most subroutines now return an array instead of an array ref

--- lib/Bio/EnsEMBL/TrackHub/Registry.pm
+++ lib/Bio/EnsEMBL/TrackHub/Registry.pm
@@ -49,6 +49,7 @@ has agent => (
   builder => '_build_agent',
 );
 
+# General setting to set registered hubs as public or hidden
 has is_public => (
   is      => 'rw',
   isa     => 'Bool',
@@ -102,6 +103,8 @@ sub _get_token {
 
 ###############################################################################
 # AUTOMATIC INSTANCE CLEANUP
+
+# Purpose: cleanly log out when the script is finished
 sub DEMOLISH {
   my $self = shift;
 
@@ -124,6 +127,9 @@ sub DEMOLISH {
 ###############################################################################
 # INSTANCE METHODS
 
+# PRIVATE METHOD
+# Purpose   : generic http request handler
+# Parameters: 1) a url 2) action (GET, DELETE, POST) 3) correct response code
 sub _request {
   my $self = shift;
   my ( $url, $action, $ok_code ) = @_;
@@ -160,33 +166,23 @@ sub _request {
     sleep $wait_time;
   }
   croak "Couldn't get a successful response after $num_repeats retries. Latest response: " . $response->status_line;
+  
+  return 1;
 }
 
-sub register_track_hubs {
-  my $self = shift;
-  my ($hubs) = @_;
+###############################################################################
+# REGISTRATION METHODS
 
-  for my $hub (@$hubs) {
-    print STDERR sprintf("Registering track hub %s with url %s (%s)\n", $hub->id, $hub->url, $self->is_public ? 'public' : 'hidden');
-    $self->new_register_track_hub(
-      id           => $hub->id,
-      url          => $hub->url,
-      assembly_map => $hub->assembly_map
-    );
-  }
-}
-
-sub new_register_track_hub {
+# INSTANCE METHOD
+# Purpose   : register a single track hub with specified parameters
+# Parameters: 1) hub id 2) hub url 2) hub assembly map
+sub register_track_hub_data {
   my $self = shift;
   my %pars = @_;
-
-  $self->register_track_hub( $pars{id}, $pars{url}, $pars{assembly_map} );
-}
-
-sub register_track_hub {
-  my $self = shift;
-
-  my ( $track_hub_id, $hub_url, $assembly_mapping, ) = @_;
+  
+  my $track_hub_id     = $pars{id};
+  my $hub_url          = $pars{url};
+  my $assembly_mapping = $pars{assembly_map};
 
   croak "No track hub id provided" if not defined $track_hub_id;
   croak "No hub.txt url provided"  if not defined $hub_url;
@@ -202,6 +198,7 @@ sub register_track_hub {
   $trackhub_content->{assemblies} = \%assemblies if %assemblies;
   $trackhub_content->{public}     = 0            if not $self->is_public;
 
+  $logger->info("Send registration request for $track_hub_id");
   my $request = POST(
     $TRACKHUB_API,
     'Content-type' => 'application/json',
@@ -228,267 +225,144 @@ sub register_track_hub {
     . $response->as_string;
 }
 
+# INSTANCE METHOD
+# Purpose   : register a list of track hubs
+# Parameters: an array of Hub objects
+sub register_track_hubs {
+  my $self = shift;
+  my @hubs = @_;
+
+  for my $hub (@hubs) {
+    print STDERR sprintf("Registering track hub %s with url %s (%s)\n", $hub->id, $hub->url, $self->is_public ? 'public' : 'hidden');
+    $self->register_track_hub_data(
+      id           => $hub->id,
+      url          => $hub->url,
+      assembly_map => $hub->assembly_map
+    );
+  }
+}
+
+# INSTANCE METHOD
+# Purpose   : update a list of track hubs to be public
+# Parameters: an array of Hub objects
+sub show_track_hubs {
+  my $self = shift;
+  my @hubs = @_;
+  
+  $self->is_public(1);
+  $self->register_track_hubs(@hubs);
+}
+
+# INSTANCE METHOD
+# Purpose   : update a list of track hubs to be hidden
+# Parameters: an array of Hub objects
+sub hide_track_hubs {
+  my $self = shift;
+  my @hubs = @_;
+  
+  $self->is_public(0);
+  $self->register_track_hubs(@hubs);
+}
+
+###############################################################################
+# DELETION METHODS
+
+# INSTANCE METHOD
+# Purpose   : unregister a track hub based on its id
+# Parameters: an id as string
+sub delete_track_hub_id {
+  my $self = shift;
+  my ($hub_id) = @_;
+  
+  $logger->info( "Deleting trackhub " . $hub_id );
+  my $url     = "$TRACKHUB_API/$hub_id";
+  $self->_request( $url, 'DELETE', 200 );
+}
+
+# INSTANCE METHOD
+# Purpose   : unregister a list track hub
+# Parameters: an array of Hub objects
+sub delete_track_hubs {
+  my $self = shift;
+  my @track_hubs = @_;
+
+  for my $track_hub (@track_hubs) {
+    croak("No track hub to delete") if (not $track_hub);
+    croak("No track hub id for deletion") if (not $track_hub->id);
+
+    $self->delete_track_hub_id($track_hub->id);
+  }
+}
+
+# INSTANCE METHOD
+# Purpose   : unregister all the currently registered hubs for this user
+# Parameters: none
 sub delete_all_track_hubs {
   my $self = shift;
   
-  my $track_hub_ids = $self->get_all_registered;
-  $self->delete_track_hubs(@$track_hub_ids);
+  $logger->info("Delete all track hubs in the registry for the user");
+  my $track_hubs = $self->get_registered;
+  $self->delete_track_hubs(@$track_hubs);
 }
 
-sub delete_track_hubs {
+###############################################################################
+# INFORMATION METHODS
+
+# INSTANCE METHOD
+# Purpose   : retrieve information about one or all track hubs
+# Parameters: a track hub name (id), or none (then all hubs are returned)
+sub get_registered {
   my $self = shift;
-  my @track_hub_ids = @_;
-
-  if (not @track_hub_ids) {
-    $logger->info("List of track hubs to delete is empty");
-    return;
-  }
-
-  my $auth_token = $self->auth_token;
-
-  my $del_count = 0;
-
-  foreach my $track_hub_id ( sort uniq @track_hub_ids ) {
-    $logger->info( "$del_count\tDeleting trackhub " . $track_hub_id );
-    my $url     = "$TRACKHUB_API/$track_hub_id";
-    my $request = DELETE($url);
-    $request->headers->header(
-      user       => $self->user,
-      auth_token => $auth_token
-    );
-    my $response      = $self->agent->request($request);
-    my $response_code = $response->code;
-
-    if ( $response->is_success and $response->code == 200 ) {
-      $del_count++;
-    }
-    else {
-      croak "Couldn't delete the track hub $track_hub_id: "
-        . $response->status_line . "\n"
-        . $response->as_string;
-    }
-  }
-  return $del_count;
+  my ($name) = @_;
+  
+  my $url = $TRACKHUB_API;
+  $url .= '/' . $name if $name;
+  my $trackhubs = $self->_request( $url, 'GET', 200 );
+  
+  return @$trackhubs;
 }
 
-sub get_all_registered {
+# INSTANCE METHOD
+# Purpose   : retrieve the name/id of all registered track hubs
+# Parameters: none
+sub get_registered_ids {
   my $self = shift;
   
-  my $trackhubs = $self->_request(
-    $TRACKHUB_API,
-    'GET',
-    200
-  );
+  my $trackhubs = $self->get_registered();
   
   my @ids = map { $_->{name} } @$trackhubs;
-  return \@ids;
+  return @ids;
 }
 
-sub get_Registry_hub_last_update { # gives the last update date(unix time) of the registration of the track hub
-
+# INSTANCE METHOD
+# Purpose   : get the latest update time of one/all registered track hubs
+# Parameters: a track hub name/id, or none
+sub get_registered_last_update {
   my $self = shift;
-
-  my $name = shift;                # track hub name, ie study_id
-
-  defined $name
-    or print
-    "Track hub name parameter required to get the track hub's last update date in the Track Hub Registry\n"
-    and return 0;
-
-  my $registry_user_name = $self->user;
-
-  my $auth_token = $self->auth_token;
-
-  my $request = GET("$SERVER/api/trackhub/$name");
-  $request->headers->header( user       => $registry_user_name );
-  $request->headers->header( auth_token => $auth_token );
-  my $response = $self->agent->request($request);
-  my $hub;
-
-  if ( $response->is_success ) {
-    $hub = from_json( $response->content );
-  }
-  else {
-
-    print
-      "\tCouldn't get Registered track hub $name with the first attempt when calling method get_Registry_hub_last_update in script "
-      . __FILE__ . "\n";
-    my $flag_success = 0;
-
-    for ( my $i = 1 ; $i <= 10 ; $i++ ) {
-
-      print "\t" . $i . ") Retrying attempt: Retrying after 5s...\n";
-      sleep 5;
-      $response = $self->agent->request($request);
-      if ( $response->is_success ) {
-        $hub          = from_json( $response->content );
-        $flag_success = 1;
-        last;
-      }
-    }
-
-    die
-      "Couldn't get track hub $name in the Registry when calling method get_Registry_hub_last_update in script: "
-      . __FILE__
-      . " line "
-      . __LINE__
-      . " I am getting code "
-      . $response->code . "\n"
-      unless $flag_success == 1;
-  }
-
-  die
-    "Couldn't find hub $name in the Registry to get the last update date when calling method get_Registry_hub_last_update in script: "
-    . __FILE__
-    . " line "
-    . __LINE__ . "\n"
-    unless $hub;
-
+  my ($name) = @_;
+  
+  my $trackhubs = $self->get_registered($name);
+  
   my $last_update = -1;
 
-  foreach my $trackdb ( @{ $hub->{trackdbs} } ) {
+  foreach my $hub (@$trackhubs) {
+    my $doc = $self->_request( $hub->{uri}, 'GET', 200 );
 
-    $request = GET( $trackdb->{uri} );
-    $request->headers->header( user       => $registry_user_name );
-    $request->headers->header( auth_token => $auth_token );
-    $response = $self->agent->request($request);
-    my $doc;
-    if ( $response->is_success ) {
-      $doc = from_json( $response->content );
+    if ( $doc->{updated} and $last_update < $doc->{updated} ) {
+      $last_update = $doc->{updated};
+    }
+    elsif ( $doc->{created} and $last_update < $doc->{created} ) {
+      $last_update = $doc->{created};
     }
     else {
-      die "\tCouldn't get trackdb at", $trackdb->{uri}
-        . " from study $name in the Registry when trying to get the last update date \n";
-    }
-
-    if ( exists $doc->{updated} ) {
-      $last_update = $doc->{updated}
-        if $last_update < $doc->{updated};
-    }
-    else {
-      exists $doc->{created}
-        or die
-        "Trackdb does not have creation date in the Registry when trying to get the last update date of study $name\n";
-      $last_update = $doc->{created}
-        if $last_update < $doc->{created};
+      croak "Trackdb does not have creation date in the Registry when trying to get the last update date of study $name\n";
     }
   }
 
-  die "Couldn't get date as expected: $last_update\n"
+  croak "Couldn't get date as expected: $last_update\n"
     unless $last_update =~ /^[1-9]\d+?$/;
 
   return $last_update;
-}
-
-sub give_all_bioreps_of_study_from_Registry {
-
-  my $self = shift;
-
-  my $name = shift;    # track hub name, ie study_id
-
-  defined $name
-    or print
-    "Track hub name parameter required to get the track hub's bioreps from the Track Hub Registry\n"
-    and return 0;
-
-  my $registry_user_name = $self->user;
-
-  my $auth_token = $self->auth_token;
-
-  my $request = GET("$SERVER/api/trackhub/$name");
-  $request->headers->header( user       => $registry_user_name );
-  $request->headers->header( auth_token => $auth_token );
-  my $response = $self->agent->request($request);
-  my $hub;
-
-  if ( $response->is_success ) {
-
-    $hub = from_json( $response->content );
-
-  }
-  else {
-
-    print
-      "\tCouldn't get Registered track hub $name with the first attempt when calling method give_all_runs_of_study_from_Registry in script "
-      . __FILE__
-      . " reason "
-      . $response->code . " , "
-      . $response->content . "\n";
-    my $flag_success = 0;
-
-    for ( my $i = 1 ; $i <= 10 ; $i++ ) {
-
-      print "\t" . $i . ") Retrying attempt: Retrying after 5s...\n";
-      sleep 5;
-      $response = $self->agent->request($request);
-      if ( $response->is_success ) {
-        $hub          = from_json( $response->content );
-        $flag_success = 1;
-        last;
-      }
-    }
-
-    die
-      "Couldn't get the track hub $name in the Registry when calling method give_all_runs_of_study_from_Registry in script: "
-      . __FILE__
-      . " line "
-      . __LINE__ . "\n"
-      unless $flag_success == 1;
-  }
-
-  die
-    "Couldn't find hub $name in the Registry to get its runs when calling method give_all_runs_of_study_from_Registry in script: "
-    . __FILE__
-    . " line "
-    . __LINE__ . "\n"
-    unless $hub;
-
-  my %runs;
-
-  foreach my $trackdb ( @{ $hub->{trackdbs} } ) {
-
-    $request = GET( $trackdb->{uri} );
-    $request->headers->header( user       => $registry_user_name );
-    $request->headers->header( auth_token => $auth_token );
-    $response = $self->agent->request($request);
-    my $doc;
-
-    if ( $response->is_success ) {
-
-      $doc = from_json( $response->content );
-
-      foreach my $sample ( keys %{ $doc->{configuration} } ) {
-        map { $runs{$_}++ }
-          keys %{ $doc->{configuration}{$sample}{members} };
-      }
-    }
-    else {
-      die "Couldn't get trackdb at ", $trackdb->{uri},
-        " from study $name in the Registry when trying to get all its runs, reason: "
-        . $response->code . " , "
-        . $response->content . "\n";
-    }
-  }
-
-  return \%runs;
-
-}
-
-sub hide_track_hubs {
-  my $self = shift;
-  my ($hubs) = @_;
-  
-  $self->is_public(0);
-  $self->register_track_hubs($hubs);
-}
-
-
-sub show_track_hubs {
-  my $self = shift;
-  my ($hubs) = @_;
-  
-  $self->is_public(1);
-  $self->register_track_hubs($hubs);
 }
 
 1;
@@ -497,4 +371,197 @@ __END__
 
 =head1 DESCRIPTION
 
+Object used to interact with a Track Hub Registry API.
+
+It can be used to register, update, delete, and get info about a track hub.
+
+=head1 ATTRIBUTES
+
+=over
+
+=item I<user>
+
+User name used to login to Track Hub Registry.
+
+=item I<password>
+
+Corresponding password for login.
+
+=item I<is_public>
+
+Whether the newly registered track hubs will be searchable (public = 1)
+or not (hidden = 0, default).
+
+=back
+
+=head1 METHODS
+
+=head2 new
+
+The object will login and retrieve an authentification the first time it is used.
+
+Usage:
+
+  $registry = Bio::EnsEMBL::TrackHub::Registry->new(
+    user     => $reg_user,
+    password => $reg_pass,
+  );
+  
+  # Make the newly registered track hubs searchable (hidden by default)
+  $registry->is_public(1);
+
+=head2 is_public
+
+Set/Get the value of the attribute is_public.
+
+Usage:
+
+  # Make the registered hubs searchable
+  $self->is_public(1);
+  # Make the registered hubs not searchable
+  $self->is_public(0);
+  # Get the value
+  my $is_public = $self->is_public();
+
+=head2 REGISTRATION
+
+=head3 B<register_track_hub_data>
+
+This is the method that actually registers a track hub. Use register_track_hubs
+instead if you already have Track Hub objects (recommended).
+
+Parameters:
+
+=over
+
+=item I<id>
+
+A unique identifier for the track hub.
+
+=item I<url>
+
+A URL pointing to the hub.txt file. Note that the Registry will only register a
+hub.txt once (all users included).
+
+=item I<assembly_map>
+
+A mapping hash ref with the key as the genome name (used in genomes.txt) and
+the value as the INSDC accession.
+
+=back
+
+Usage:
+
+  $self->register_track_hub_data(
+    id           => $id,
+    url          => $url,
+    assembly_map => $assembly_map
+  );
+
+=head3 B<register_track_hubs>
+
+The best and simplest way to register a Hub.
+
+Parameters: an array of Hub objects.
+
+Usage:
+
+  # One track hub
+  $self->register_track_hubs($hub);
+  # Several track hubs
+  $self->register_track_hubs(@hubs);
+
+=head3 B<show_track_hubs>
+
+Update a list of track hubs and make them public (searchable).
+
+Parameters: an array of Hub objects.
+
+Usage:
+
+  $self->show_track_hubs(@hubs);
+
+=head3 B<hide_track_hubs>
+
+Update all currently registered track hubs and make them hidden (unsearchable).
+
+Parameters: an array of Hub objects.
+
+Usage:
+
+  $self->hide_track_hubs(@hubs);
+
+
+=head2 DELETION
+
+=head3 B<delete_track_hub_id>
+
+Unregister one track hub based on its name/id.
+
+Parameters: a string id.
+
+Usage:
+
+  $self->delete_track_hub_id($name);
+
+=head3 B<delete_track_hubs>
+
+Unregister one or several track hubs (the id is extracted from the Hub objects).
+
+Parameters: an array of Hub objects.
+
+Usage:
+
+  $self->delete_track_hubs(@hubs);
+
+=head3 B<delete_all_track_hubs>
+
+Unregister all registered track hubs for the current user. Use carefully.
+
+Parameters: none
+
+Usage:
+
+  $self->delete_all_track_hubs();
+
+=head2 INFORMATION
+
+=head3 B<get_registered>
+
+Get information about one or all registered track hubs for the current user.
+
+Parameters: none or the name/id of a registered track_hub
+
+Returns   : array of track hub hash informations
+
+Usage:
+
+  my @metadata       = $self->get_registered();
+  my ($hub_metadata) = $self->get_registered($id);
+
+=head3 B<get_registered_ids>
+
+Get the list of ids of all registered track hubs for the current user.
+
+Parameters: none
+
+Returns   : array of ids (strings)
+
+Usage:
+
+  my @ids = $self->get_registered_ids();
+
+=head3 B<get_registered_last_update>
+
+Get the time of the latest change to all track hubs of the user.
+
+Parameters: none
+
+Returns   : Unix time (integer)
+
+Usage:
+
+  my $last_time = $self->get_registered_last_time();
+
+=cut
 
--- t/70.Registry.t
+++ t/70.Registry.t
@@ -8,7 +8,7 @@ use Test::More qw(no_plan);
 use Test::Exception;
 
 use Log::Log4perl qw( :easy );
-Log::Log4perl->easy_init($DEBUG);
+#Log::Log4perl->easy_init($DEBUG);
 my $logger = get_logger();
 use Data::Dumper;
 
@@ -21,6 +21,7 @@ $ua->timeout(5);
 use FindBin;
 use lib $FindBin::Bin . '/../lib';
 use_ok('Bio::EnsEMBL::TrackHub::Registry');
+use_ok('Bio::EnsEMBL::TrackHub::Hub');
 
 # -----
 # test constructor
@@ -36,9 +37,7 @@ SKIP: {
     if not ($user and $pass);
 
   # First, test the login
-  dies_ok {
-    my $registry = Bio::EnsEMBL::TrackHub::Registry->new;
-  }
+  dies_ok { my $registry = Bio::EnsEMBL::TrackHub::Registry->new; }
   "Login without credentials fails";
 
   # Wrong credentials
@@ -51,8 +50,7 @@ SKIP: {
   "Login with wrong credentials fails";
 
   # Actual credentials
-  ok(
-    my $registry = Bio::EnsEMBL::TrackHub::Registry->new(
+  ok( my $registry = Bio::EnsEMBL::TrackHub::Registry->new(
       user     => $user,
       password => $pass
     ),
@@ -65,46 +63,46 @@ SKIP: {
     'The object constructed is of my class type'
   );
 
-  dies_ok {
-    Bio::EnsEMBL::TrackHub::Registry->new("blabla");
-  }
-  'Wrong object construction dies';
+  dies_ok { Bio::EnsEMBL::TrackHub::Registry->new("blabla"); }
+    'Wrong object construction dies';
 
-  ok( $registry->is_public == 0,    "The registry is not public by default" );
+  ok( $registry->is_public    == 0, "The registry is not public by default" );
   ok( $registry->is_public(1) == 1, "Set the registry to public mode" );
   ok( $registry->is_public(0) == 0, "Set the registry back to private mode" );
   
   # Check registered
   ok(
-    my $reg = $registry->get_registered(),
+    (my @reg = $registry->get_registered()) == 0,
     "Can get list of registered hubs"
   );
   
   # Not a test, but a check: we don't want to touch non-test track hubs!
   croak "The current registry for the test user is not empty.
-  Please make sure that the test account has no hubs (this is to prevent mistakes)." if not $reg or @$reg > 0;
+  Please make sure that the test account has no hubs (this is to prevent mistakes)." if @reg > 0;
 
   # Register 1 trackhub
   dies_ok {
-    $registry->register_track_hub(
-      "trackhub_id",
-      "https://wrong.address/hub.txt"
+    $registry->register_track_hub_data(
+      id  => "trackhub_id",
+      url => "https://wrong.address/hub.txt"
     );
   }
-  "Can't register a trackhub with wrong hub.txt url";
+  "Can't register a trackhub with wrong parameters";
 
-  my $th_id = 'VBRNAseq_SRP021068';
-  my $hub_url =
-    'http://www.ebi.ac.uk/~mbarba/testing/eg-trackhub/VBRNAseq_SRP021068/hub.txt';
+  my $th_id       = 'VBRNAseq_SRP021068';
+  my $hub_root    = 'http://www.ebi.ac.uk/~mbarba/testing/eg-trackhub/VBRNAseq_SRP021068';
+  my $hub_url     = "$hub_root/hub.txt";
+  my $genome_name = 'AminM1';
+  my $insdc       = 'GCA_000349025.1';
   my %assemblies = (
-    'AminM1' => 'GCA_000349025.1',
+    $genome_name => $insdc,
   );
 
   # Check that the file is accessible, otherwise skip this test
   SKIP: {
     skip "hub.txt url in not accessible" if not head($hub_url);
     ok(
-      $registry->register_track_hub(
+      $registry->register_track_hub_data(
         id           => $th_id,
         url          => $hub_url,
         assembly_map => \%assemblies
@@ -112,20 +110,45 @@ SKIP: {
       "Can register a trackhub with correct data"
     );
 
-    ok(
-      my $reg = $registry->get_registered(),
-      "Can get list of registered hubs"
-    );
+    # Check that the registration worked
+    ok( my $reg = $registry->get_registered(),
+      "Can get list of registered hubs" );
 
-    ok(
-      @$reg == 1,
-      "The list contains 1 registered hub",
-    );
+    ok( @$reg == 1,
+      "The list contains 1 registered hub" );
 
-    ok(
-      $registry->delete_track_hubs($th_id),
-      "Can delete a trackhub"
+    # Get information
+    ok( my @meta = $registry->get_registered(), "Get track hubs data");
+
+    ok( my $last_time     = $registry->get_registered_last_update(), "Get last update time");
+    ok( $last_time =~ /^\d+$/, "Last update time is in a correct format");
+
+    # Delete this specific track hub
+    ok( $registry->delete_track_hubs($th_id),
+      "Can delete a trackhub" );
+
+    # Register the hub again with a different method
+    my $hub = Bio::EnsEMBL::TrackHub::Hub->new(
+      id          => $th_id,
+      shortLabel  => 'Trackhub test1',
+      longLabel   => 'Trackhub test1 long label',
+      root_dir    => $hub_root,
     );
+    my $genome = Bio::EnsEMBL::TrackHub::Hub::Genome->new(
+      id    => $genome_name,
+      insdc => $insdc,
+    );
+    $hub->add_genome($genome);
+    ok( $registry->register_track_hubs($hub),
+      "Can register a trackhub with a Hub object" );
+
+    # And delete all trackhubs for the user
+    ok( $registry->delete_all_track_hubs(),
+      "Can delete all trackhubs" );
+
+    # No more track hubs
+    ok( my @regend = $registry->get_registered(), "Check registered tracks after complete deletion");
+    ok( @regend == 0, "There are no more tracks" );
   }
 }
 
